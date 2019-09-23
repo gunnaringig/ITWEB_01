b11231b72e86a870b20ce3202ad3f83fca299c92
Gunnar Ingi Geirsson <gunnar.ingi.geirsson@gmail.com>
2019-09-23 13:42:59 +0200

    beautify

--- ITWEB_01/app.js
+++ ITWEB_01/app.js
@@ -6,13 +6,15 @@ import {MDCTextField} from '@material/textfield';
 // instatiate materialdesign textfield
 const textField = new MDCTextField(document.querySelector('.mdc-text-field'));
 const buttonRipple = new MDCRipple(document.querySelector('.mdc-button')); */
-
+var favicon = require('serve-favicon');
 // Routes
 var routes = require('./routes/index');
 var users = require('./routes/users');
 
 // Init app
 var app = express();
+
+app.use(favicon(path.join(__dirname, '/static', 'favicon.ico')));
 
 app.set('views', path.join(__dirname,'views'));
 app.engine('html', require('ejs').renderFile)
